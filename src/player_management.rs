6b948e5b2006013555b7695cc0ee5b2d2e0eb63c
Emstrevk <emil.strandvik@gmail.com>
2021-01-23 19:13:50 +0200

    potential bottleneck snipe in note sending

--- src/player_management.rs
+++ src/player_management.rs
@@ -29,13 +29,9 @@ impl PlayerManager {
     }
 
     pub fn force_reset(&self) {
-
-        // TODO: If operation lasts longer than a tick, the lock can hang main loop:?
-
         for (_, player) in self.sequence_players.lock().unwrap().iter() {
             player.lock().unwrap().shift_queue();
         }
-
         self.beat_counter.lock().unwrap().set(0.0);
     }
 
@@ -44,8 +40,8 @@ impl PlayerManager {
 
         self.beat_counter.lock().unwrap().update(| v| v + beats_elapsed);
 
-        // TODO: Cloning might be a good idea here; we only need the notes at the time of the lock..?
-        //  Update: Actually get_next is destructive so that won't work
+        // TODO: The actual sending seems to be a rather heavy operation which can result in
+        //  annoying hiccups on very large player set sizes (tested on >100)
         for (name, player) in self.sequence_players.lock().unwrap().iter() {
             let notes_on_time = player.lock().unwrap().get_next(
                 self.beat_counter.lock().unwrap().clone().into_inner()
@@ -56,23 +52,30 @@ impl PlayerManager {
             if !notes_on_time.is_empty() {
 
                 let output = player.lock().unwrap().target_output.lock().unwrap().clone();
+                let thread_player = player.clone();
+                let thread_client = self.rest_client.clone();
 
                 println!("Sending note on beat: {}", self.beat_counter.lock().unwrap().clone().into_inner());
 
-                match player.lock().unwrap().player_target {
-                    PlayerTarget::PROSC => {
-                        self.rest_client.clone().lock().unwrap()
-                            .post_prosc_notes( &output, notes_on_time.clone());
-                    },
-                    PlayerTarget::MIDI => {
-                        self.rest_client.clone().lock().unwrap()
-                            .post_midi_notes( &output, notes_on_time.clone());
-                    },
-                    PlayerTarget::PROSC_SAMPLE => {
-                        self.rest_client.clone().lock().unwrap()
-                            .post_prosc_samples( &output, notes_on_time.clone());
-                    },
-                }
+                // Not sure how much this threading matters, but as I understand http ALWAYS waits for a response.
+                // Since the response doesn't really matter to the sending thread, we just leave the send
+                // in a thread and move on...
+                thread::spawn(move || {
+                    match thread_player.player_target {
+                        PlayerTarget::PROSC => {
+                            thread_client.lock().unwrap()
+                                .post_prosc_notes( &output, notes_on_time.clone());
+                        },
+                        PlayerTarget::MIDI => {
+                            thread_client.lock().unwrap()
+                                .post_midi_notes( &output, notes_on_time.clone());
+                        },
+                        PlayerTarget::PROSC_SAMPLE => {
+                            thread_client.unwrap()
+                                .post_prosc_samples( &output, notes_on_time.clone());
+                        },
+                    }
+                });
 
             }
         }
