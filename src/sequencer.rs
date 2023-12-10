6c93ac03f4a2e3073e65e47f2abb330b8a9e3637
emstrevk <emil.strandvik@protonmail.com>
2023-12-10 16:33:33 +0100

    Working reset and floating point issues fixed

--- src/sequencer.rs
+++ src/sequencer.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+use bigdecimal::{BigDecimal, Zero}; // Floating point arithmetic is unsuitable for exact calculations 
+
 /*
 
    Rewrite of sequencer code in queue.rs.
@@ -6,32 +9,54 @@
        2. Generic (entries use <beat: float, T>)
        3. Transparent (minimal amount of mutation during regular operations - e.g. don't erase the active sequence)
 
+
+    GENERAL NOTE ON OVERSHOOT
+        - When looping a single sequencer, the overshoot is simple to justify and calculate: 
+            - You don't want to lose any progress towards the first note from the last 
+        - When dealing with many sequencers, it's a lot trickier. 
+            - If several of them are waiting for a final one to complete, the overshoot is effectively that of the 
+                final sequencer for ALL OTHER SEQUENCERS AS WELL; the others cannot use their own overshoot values 
+
 */
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct SequencerEntry<T: Clone> {
-    pub beat: f32,
+    pub trigger_beat: BigDecimal,
     pub contents: T,
 }
 
 pub struct Sequencer<T: Clone> {
     pub active_sequence: Vec<SequencerEntry<T>>,
     pub queued_sequence: Vec<SequencerEntry<T>>,
-    pub current_beat: f32,
-    pub processed_beats: Option<f32>,
-    pub end_beat: f32,
+    pub current_beat: BigDecimal,
+    pub processed_beats: Option<BigDecimal>,
+    pub end_beat: BigDecimal,
+    pub queue_end_beat: BigDecimal
 }
 
 impl<T: Clone> Sequencer<T> {
 
-    pub fn reset(&mut self) {
-        self.current_beat = 0.0;
-        self.processed_beats = None; 
-        self.active_sequence = self.queued_sequence.clone();
-        // TODO: See how shift_queue handles end_beat  
+    pub fn new() -> Sequencer<T> {
+        Sequencer {
+            active_sequence: vec![],
+            queued_sequence: vec![],
+            current_beat: BigDecimal::zero(),
+            processed_beats: None,
+            end_beat: BigDecimal::zero(),
+            queue_end_beat: BigDecimal::zero()
+        }
     }
 
-    pub fn tick(&mut self, beats: f32) -> Vec<T> {
+    // Overshoot is the amount of beats you've already started counting on the new sequence, e.g. by having the last tick amount
+    //  overshoot the end_beat by n amount
+    pub fn reset(&mut self, overshoot: BigDecimal) {
+        self.current_beat = overshoot;
+        self.processed_beats = None; 
+        self.active_sequence = self.queued_sequence.clone();
+        self.end_beat = self.queue_end_beat.clone();
+    }
+
+    pub fn tick(&mut self, beats: BigDecimal) -> Vec<T> {
 
 
         // Finished sequences stop ticking
@@ -43,13 +68,13 @@ impl<T: Clone> Sequencer<T> {
                 .active_sequence
                 .iter()
                 .filter(|n| {
-                    &n.beat <= &self.current_beat && match &self.processed_beats { Some(value) => &n.beat > value, None => true }
+                    &n.trigger_beat <= &self.current_beat && match &self.processed_beats { Some(value) => &n.trigger_beat > value, None => true }
                 })
                 .map(|n| n.clone().contents.clone())
                 .collect();
     
             // Note that entries up until this beat have been tick-returned and should not be returned again on later current_beats
-            self.processed_beats = Some(self.current_beat);
+            self.processed_beats = Some(self.current_beat.clone());
     
             candidates
     
@@ -63,49 +88,86 @@ impl<T: Clone> Sequencer<T> {
         &self.current_beat >= &self.end_beat
     }
 
+    // Use to check by how much tick() has pushed current_beat past end_beat. 
+    pub fn get_overshoot(&self) -> BigDecimal {
+        return match &self.current_beat > &self.end_beat {true => &self.current_beat - &self.end_beat, false => BigDecimal::zero()}; 
+    }
+
+    /*
+        NOTE: This presumes that new_queue already has its entries arranged by trigger_beat rather than beats_reserved, which was common in 
+            an earlier implementation. 
+    */
+    pub fn queue(&mut self, new_queue: Vec<SequencerEntry<T>>, end_beat: BigDecimal) {
+        self.queue_end_beat = end_beat.clone();
+        self.queued_sequence = new_queue.clone();
+    }
 
 
 }
 
 mod tests {
+    use std::str::FromStr;
+
     use super::SequencerEntry;
     use super::Sequencer;
+    use bigdecimal::BigDecimal;
+
+    #[test]
+    fn reset_test() {
+        let entries: Vec<SequencerEntry<&str>> = vec![
+            SequencerEntry {trigger_beat: BigDecimal::from_str("0.0").unwrap(), contents:"one"},    
+            SequencerEntry {trigger_beat: BigDecimal::from_str("0.2").unwrap(), contents:"two"},    
+            SequencerEntry {trigger_beat: BigDecimal::from_str("1.0").unwrap(), contents:"three"},    
+        ];
+
+        let mut sequencer = Sequencer::new();
+        sequencer.queue(entries, big("1.5"));
+        sequencer.reset(big("0.3"));
+
+        assert_eq!(sequencer.current_beat, BigDecimal::from_str("0.3").unwrap());
+        assert_eq!(sequencer.processed_beats, None);
+
+        assert_eq!(sequencer.tick(big("0.6")), vec!["one", "two"]);
+        assert_eq!(sequencer.current_beat, BigDecimal::from_str("0.9").unwrap());
+        assert_eq!(&sequencer.processed_beats.clone().unwrap(), &BigDecimal::from_str("0.9").unwrap());
+
+    }
+
+    fn big(inp: &str) -> BigDecimal {
+        BigDecimal::from_str(inp).unwrap()
+    }
 
     #[test]
     fn tick_test() {
         let entries: Vec<SequencerEntry<&str>> = vec![
-            SequencerEntry {beat: 0.0, contents:"one"},    
-            SequencerEntry {beat: 0.5, contents:"two"},    
-            SequencerEntry {beat: 1.5, contents:"three"},    
+            SequencerEntry {trigger_beat: big("0.0"), contents:"one"},    
+            SequencerEntry {trigger_beat: big("0.5"), contents:"two"},    
+            SequencerEntry {trigger_beat: big("1.5"), contents:"three"},    
         ];
 
-        let mut sequencer: Sequencer<&str> = Sequencer {
-            active_sequence: entries.clone(),
-            queued_sequence: entries.clone(),
-            current_beat: 0.0,
-            processed_beats: None,
-            end_beat: 3.0
-        };
+        let mut sequencer = Sequencer::new();
+        sequencer.queue(entries, big("3.0"));
+        sequencer.reset(big("0.0"));
 
-        assert_eq!(sequencer.tick(0.25), vec!["one"]);
-        assert_eq!(sequencer.current_beat, 0.25);
-        assert_eq!(sequencer.processed_beats, Some(0.25));
+        assert_eq!(sequencer.tick(big("0.25")), vec!["one"]);
+        assert_eq!(sequencer.current_beat, big("0.25"));
+        assert_eq!(sequencer.processed_beats.clone().unwrap(), big("0.25"));
 
-        assert_eq!(sequencer.tick(0.25), vec!["two"]);
-        assert_eq!(sequencer.current_beat, 0.5);
-        assert_eq!(sequencer.processed_beats, Some(0.5));
-        assert_eq!(sequencer.tick(0.25).is_empty(), true);
-        assert_eq!(sequencer.tick(0.25).is_empty(), true);
-        assert_eq!(sequencer.tick(0.25).is_empty(), true);
-        assert_eq!(sequencer.tick(0.25), vec!["three"]);
-        assert_eq!(sequencer.tick(1.4).is_empty(), true);
-        assert_eq!(sequencer.current_beat, 2.9);
+        assert_eq!(sequencer.tick(big("0.25")), vec!["two"]);
+        assert_eq!(sequencer.current_beat, big("0.5"));
+        assert_eq!(sequencer.processed_beats.clone().unwrap(), big("0.5"));
+        assert_eq!(sequencer.tick(big("0.25")).is_empty(), true);
+        assert_eq!(sequencer.tick(big("0.25")).is_empty(), true);
+        assert_eq!(sequencer.tick(big("0.25")).is_empty(), true);
+        assert_eq!(sequencer.tick(big("0.25")), vec!["three"]);
+        assert_eq!(sequencer.tick(big("1.4")).is_empty(), true);
+        assert_eq!(sequencer.current_beat, big("2.9"));
         assert_eq!(sequencer.is_finished(), false);
-        assert_eq!(sequencer.tick(0.3).is_empty(), true);
-        assert_eq!(sequencer.current_beat, 3.2);
+        assert_eq!(sequencer.tick(big("0.3")).is_empty(), true);
+        assert_eq!(sequencer.current_beat, big("3.2"));
         assert_eq!(sequencer.is_finished(), true);
-        assert_eq!(sequencer.tick(0.3).is_empty(), true);
-        assert_eq!(sequencer.current_beat, 3.2);
+        assert_eq!(sequencer.tick(big("0.3")).is_empty(), true);
+        assert_eq!(sequencer.current_beat, big("3.2"));
         assert_eq!(sequencer.is_finished(), true);
 
     }
